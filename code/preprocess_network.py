79aeebbc7544df9e7fd01c2fea8915ad60d58978
huilisabrina <30443440+huilisabrina@users.noreply.github.com>
2020-05-10 19:44:49 -0400

    Add cluster split to the preprocessing script

--- code/preprocess_network.py
+++ code/preprocess_network.py
@@ -9,43 +9,81 @@
 import random
 import numpy as np
 import pandas as pd
+import itertools as it
 
 # Load raw (cleaned) network data (edge_list.csv is the network_data.csv)
 network_data="edge_list.csv"
 df = pd.read_csv(network_data, sep=",", index_col=False)
 
 # Randomly select 100 lines from the original dataset for testing
-df = df.sample(n=100, random_state=1)
+# df = df.sample(n=100, random_state=1)
+
+def gen_v_and_e(df):
+
+    # Construct the vertices table
+    id_list_1 = list(df['ID1'])
+    id_list_2 = list(df['ID2'])
+
+    temp = list(set(id_list_1) | set(id_list_2))
+    id_list = [x.split("_")[0] for x in temp]
+    cluster_list = [x.split("_")[1] for x in temp]
+    v_df = pd.DataFrame(data={"id": id_list, "cluster": cluster_list})
+    v_df = v_df[["id", "cluster"]]
+
+    # Construct the edges table
+    e_df = df[["ID1", "ID2", "STUDYNUM"]]
+    e_df["src"] = e_df["ID1"].str.slice(start=0, stop=-2, step=1)
+    e_df["dst"] = e_df["ID2"].str.slice(start=0, stop=-2, step=1)
+    e_df = e_df[["src","dst"]]
+
+    return v_df, e_df
+
+def gen_v(df_e):
+
+    id_list_1 = list(df_e['src'])
+    id_list_2 = list(df_e['dst'])
+
+    id_list = list(set(id_list_1) | set(id_list_2))
+    v_df = pd.DataFrame(data={"id": id_list})
+
+    return v_df
 
 #### =======================================
 ####  BASIC PREP (USE DATASET AT FACE VALUE)
 #### =======================================
 
-# Construct the vertices table
-id_list_1 = list(df['ID1'])
-id_list_2 = list(df['ID2'])
-
-temp = list(set(id_list_1) | set(id_list_2))
-id_list = [x.split("_")[0] for x in temp]
-cluster_list = [x.split("_")[1] for x in temp]
-v_df = pd.DataFrame(data={"id": id_list, "cluster": cluster_list})
-v_df = v_df[["id", "cluster"]]
-
-# Construct the edges table
-e_df = df[["ID1", "ID2", "STUDYNUM"]]
-e_df["src"] = e_df["ID1"].str.slice(start=0, stop=-2, step=1)
-e_df["dst"] = e_df["ID2"].str.slice(start=0, stop=-2, step=1)
-e_df = e_df[["src","dst"]]
-
-# Save to files
+# all clusters together (original)
+v_df, e_df = gen_v_and_e(df)
 v_df.to_csv("v_orig.txt", sep='\t', index=False, na_rep="NA")
 e_df.to_csv("e_orig.txt", sep='\t', index=False, na_rep="NA")
 
-#### =======================================
-####  CHANGE NETWORK AT OUR DISCRETION
-#### =======================================
+# Split up data by study number, defined through "cluster" column
+for cluster in range(1,9):
 
-# CHANGE THE SIZE OF THE NETWORK DATA...
+    df_cluster = df.loc[df['STUDYNUM'] == cluster]
 
-# ADD RANDOM EDGES....
+    v_df, e_df = gen_v_and_e(df_cluster)
+    v_df.to_csv("v_orig_cluster_" + str(cluster) + ".txt", sep='\t', index=False, na_rep="NA")
+    e_df.to_csv("e_orig_cluster_" + str(cluster) + ".txt", sep='\t', index=False, na_rep="NA")
 
+    print("Cluster: {} \n Number of vertices: {} \n Number of edges: {}".format(cluster, v_df.shape[0], e_df.shape[0]))
+
+# Modify the connectivity of the graph (simulated data for testing assumptions)
+cluster = 4
+df_cluster = df.loc[df['STUDYNUM'] == cluster]
+v_df, _ = gen_v_and_e(df_cluster)
+
+all_comb = list(it.combinations(list(v_df["id"]), 2))
+e_df_all = pd.DataFrame(data={"src": [x[0] for x in all_comb], "dst": [x[1] for x in all_comb]})
+v_df.to_csv("v_cluster_" + str(cluster) + "_all.txt", sep='\t', index=False, na_rep="NA")
+e_df_all.to_csv("e_cluster_" + str(cluster) + "_all.txt", sep='\t', index=False, na_rep="NA")
+
+e_df_med = e_df_all.sample(n=int(e_df_all.shape[0]*0.75), random_state=1)
+v_df_med = gen_v(e_df_med)
+v_df.to_csv("v_cluster_" + str(cluster) + "_med.txt", sep='\t', index=False, na_rep="NA")
+e_df_med.to_csv("e_cluster_" + str(cluster) + "_med.txt", sep='\t', index=False, na_rep="NA")
+
+e_df_low = e_df_all.sample(n=int(e_df_all.shape[0]*0.25), random_state=1)
+v_df_low = gen_v(e_df_low)
+v_df.to_csv("v_cluster_" + str(cluster) + "_low.txt", sep='\t', index=False, na_rep="NA")
+e_df_low.to_csv("e_cluster_" + str(cluster) + "_low.txt", sep='\t', index=False, na_rep="NA")
